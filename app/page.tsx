e0dc8d5bd8a63c3d1b1fe6ddfc16b560d06235fe
Jonatha Fernandes <jonathafernandes@live.com>
2024-08-08 16:31:10 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+
+import Header from "./_components/header"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <Button>Click me</Button>
-  );
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, João!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
