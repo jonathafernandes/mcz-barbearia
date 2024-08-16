739bee6a8c9bcbd5d2339f34cba35919ef6e8a2b
Jonatha Fernandes <jonathafernandes@live.com>
2024-08-16 20:48:36 -0300

    feat: add modal login

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -2,9 +2,17 @@ import { Button } from "./ui/button"
 import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
+// import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -12,14 +20,35 @@ const SidebarSheet = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogOutIcon size={18} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça o login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conect-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image src="/google.svg" alt="Google" width={18} height={18} />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://github.com/jonathafernandes.png" alt="" />
         </Avatar>
         <div>
           <p className="font-bold">João Maria</p>
           <p className="text-xs">joao@exemple.com</p>
-        </div>
+        </div> */}
       </div>
       <div className="flex flex-col gap-2 border-b border-solid py-5">
         <SheetClose asChild>
