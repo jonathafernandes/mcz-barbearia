dfe86a1db70af47d54d6342619f10396c3ab8f57
Jonatha Fernandes <jonathafernandes@live.com>
2024-08-31 09:53:54 -0300

    feat: changes hours according to day

--- app/_helpers/hours.ts
+++ app/_helpers/hours.ts
@@ -1,16 +1,35 @@
 import { addMinutes, setHours, setMinutes, format } from "date-fns"
 
 export function generateDayTimeList(date: Date): string[] {
-  const starTime = setMinutes(setHours(date, 9), 0)
-  const endTime = setMinutes(setHours(date, 21), 0)
+  let startTime, endTime
+
+  switch (date.getDay()) {
+    case 0:
+      startTime = setMinutes(setHours(date, 8), 0)
+      endTime = setMinutes(setHours(date, 14), 0)
+      break
+    case 6:
+      startTime = setMinutes(setHours(date, 13), 0)
+      endTime = setMinutes(setHours(date, 22), 0)
+      break
+    default:
+      startTime = setMinutes(setHours(date, 18), 0)
+      endTime = setMinutes(setHours(date, 22), 0)
+      break
+  }
+
   const interval = 45
   const timesList: string[] = []
 
-  let currentTime = starTime
+  let currentTime = startTime
 
-  while (currentTime <= endTime) {
+  while (currentTime < endTime) {
     timesList.push(format(currentTime, "HH:mm"))
     currentTime = addMinutes(currentTime, interval)
+  }
+
+  if (currentTime === endTime) {
+    timesList.push(format(endTime, "HH:mm"))
   }
 
   return timesList
